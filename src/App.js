5d430e20f6e2ffcf16ba627b122fa4a20640fdeb
Dominic Mason <masond4@vcu.edu>
2023-09-02 02:13:33 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import Header from './Header';
 
 function App() {
   return (
@@ -22,8 +23,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Header />
       <Routes>
-        <Route exact path="/home" component={Home} />
+        <Route path="/home" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
       </Routes>
