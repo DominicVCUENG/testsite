90418541582904727549905cd63324d1f6d2db18
Dominic Mason <masond4@vcu.edu>
2023-09-02 02:07:08 -0400

    Fixed router components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
@@ -22,11 +22,11 @@ function App() {
           Learn React
         </a>
       </header>
-      <Switch>
+      <Routes>
         <Route exact path="/home" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
-      </Switch>
+      </Routes>
     </div>
   );
 }
